aee6e39060af999c141fa2b92bf49a8b26696b0c
linxinliang <1285331961@qq.com>
2017-06-28 18:27:15 +0800

    ssss
    sss

---
+++ app.js
@@ -0,0 +1,38 @@
+const http = require('http')
+
+const homePage = `
+<!DOCTYPE html>
+<html>
+    <head>
+     <meta charset='utf-8'>
+     <title>部署测试（亮）</title>
+     <style>
+        *{
+            padding:0;
+            margin:0;
+        }
+        body {
+            padding:30px 0;
+            text-align:center;
+            font-size:16px;
+            background-color:#333;
+        }
+        h1{
+            color:#fff;
+        }
+     </style>
+    </head>
+    <body>
+        <h1>部署测试</h1>
+        
+    </body>
+</html>
+`
+http.createServer((req, res) => {
+ res.statuCode = 200
+ res.setHeader('Conten-Type', 'text/html')
+ res.end(homePage)
+})
+.listen(3000, () => {
+ console.log('Server Running At 3000')
+})
